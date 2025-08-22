7e67d690532d5d72d307bfc36987a0a3187fdaa1
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-08-22 10:09:13 +0530

    added files

--- MaxCircularSubarraySum.java
+++ MaxCircularSubarraySum.java
@@ -1,12 +1,31 @@
-public class MaxCircularSubarraySum {
-  static int maxCircularSubarraySum(int[] arr) {
-      int maxKadane = KadanesAlgo.maxSubarraySum(arr);
-      int arraySum = 0;
-      for (int i = 0; i < arr.length; i++) {
-          arraySum += arr[i];
-          arr[i] = -arr[i];
-      }
-      int maxWrap = arraySum + KadanesAlgo.maxSubarraySum(arr);
-      return Math.max(maxKadane, maxWrap);
-  }
+class Solution {
+    public int maxCircularSum(int arr[]) {
+        int totalSum = 0;
+        int maxNormal = kadane(arr);
+        
+        // Edge case: all numbers are negative
+        if (maxNormal < 0) return maxNormal;
+        
+        int minSubarraySum = 0;
+        int currMin = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            totalSum += arr[i];
+            currMin = Math.min(arr[i], currMin + arr[i]);
+            minSubarraySum = Math.min(minSubarraySum, currMin);
+        }
+        
+        int maxCircular = totalSum - minSubarraySum;
+        return Math.max(maxNormal, maxCircular);
+    }
+    
+    // Kadane's Algorithm for max subarray sum
+    private int kadane(int[] arr) {
+        int maxSoFar = arr[0], currMax = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            currMax = Math.max(arr[i], currMax + arr[i]);
+            maxSoFar = Math.max(maxSoFar, currMax);
+        }
+        return maxSoFar;
+    }
 }
